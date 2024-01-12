433d1d00a75d1086877e8e2414ae3b7cdaf4619f
Riccardo Potalivo <riccardopotalivo@gmail.com>
2024-01-12 17:46:37 +0100

    migrate & seeder set

--- database/migrations/2024_01_12_145347_create_projects_table.php
+++ database/migrations/2024_01_12_145347_create_projects_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +12,12 @@ return new class extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
+
+            $table->string('title', 150)->unique();
+            $table->string('name', 200)->unique();
+            $table->text('description')->nullable();
+            $table->string('repository', 200)->required();
+
             $table->timestamps();
         });
     }
--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Project;
+use Illuminate\Support\Str;
 
 class ProjectSeeder extends Seeder
 {
@@ -12,6 +14,17 @@ class ProjectSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $projects = config('db.projects');
+        foreach ($projects as $project) {
+            $newProject = new Project();
+
+            $newProject->title = $project['title'];
+            $newProject->name = $project['name'];
+            $newProject->description = $project['description'];
+            $newProject->repository = $project['repository'];
+            $newProject->slug = Str::slug($project['title'] . '-');
+
+            $newProject->save();
+        }
     }
 }
